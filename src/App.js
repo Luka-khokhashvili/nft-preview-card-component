c30755ecc9bdd6816ceb47a7363912a02c83208a
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2024-02-23 20:51:15 +0400

    main commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import mainLogo from "./images/image-equilibrium.jpg";
+import hoverIcon from "./images/icon-view.svg";
+import ethereumIcon from "./images/icon-ethereum.svg";
+import clockIcon from "./images/icon-clock.svg";
+import userAvatar from "./images/image-avatar.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-body">
+        <div className="main-logo-div">
+          <img src={mainLogo} alt="main logo" className="main-logo" />
+          <div className="hover-container"></div>
+          <img src={hoverIcon} alt="hover eye icon" className="hover-icon" />
+        </div>
+        <InfoBody />
+      </div>
+    </div>
+  );
+}
+
+function InfoBody() {
+  return (
+    <div className="info-body-div">
+      <Info />
+      <User />
+    </div>
+  );
+}
+
+function Info() {
+  return (
+    <div className="info-section">
+      <h3 className="info-heading">Equilibrium #3429</h3>
+      <p className="info-paragraph">
+        Our Equilibrium collection promotes balance and calm.
+      </p>
+      <InfoPrice />
+    </div>
+  );
+}
+
+function InfoPrice() {
+  return (
+    <div className="info-price-div">
+      <div className="info-box">
+        <img src={ethereumIcon} alt="ethereum icon" className="icon" />
+        <p className="price-paragraph">0.041 ETH</p>
+      </div>
+      <div className="info-box">
+        <img src={clockIcon} alt="clock icon" className="clock-icon" />
+        <p className="regular-paragraph">3 days left</p>
+      </div>
+    </div>
+  );
+}
+
+function User() {
+  return (
+    <div className="user-div">
+      <img src={userAvatar} alt="user avatar" className="user-avatar" />
+      <p className="regular-paragraph">
+        Creation of
+        <span className="username"> Jules Wyvern</span>
+      </p>
     </div>
   );
 }
